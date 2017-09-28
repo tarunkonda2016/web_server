7f4f0120e72a8d1b75d4f43513079b6129c90322
tarun konda <tarunkonda2016@gmail.com>
2017-09-28 16:59:01 +0530

    middle ware

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 var express = require('express');
+var middleware = require('./middle-ware.js');
 var app = express();
 var port = 3000;
 
-var middleware = {
-	requireAuthentication : function(req,res,next){
-		console.log('private route hit');
-		next();
-	},
-	logger : function(req,res,next){
-		console.log(new Date().toString()+req.method+req.originalUrl);
-		next();
-	}
-};
+
 
 
 app.use(middleware.logger);
